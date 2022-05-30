2ab30c1b06c0e3dc45d67bc3c3ce991c3cc67cd4
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-30 15:22:54 +0700

    commit

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // creating base URL
 export const API = axios.create({
-  baseURL: process.env.SERVER_URL || "https://dumbsound-fedryan.herokuapp.com/api/v1/" || "http://localhost:8080/api/v1",
+  baseURL: process.env.SERVER_URL || "https://dumbsound-fedryan.herokuapp.com/api/v1" || "http://localhost:8080/api/v1",
 });
 
 export const setAuthToken = (token) => {
