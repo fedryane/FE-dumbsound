44fb0c361e70416caeb3f3b39e5115d4ef31273d
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-30 21:49:05 +0700

    commit

--- src/components/modal/Register.js
+++ src/components/modal/Register.js
@@ -69,6 +69,9 @@ function Register(props) {
           fullname: "",
           email: "",
           password: "",
+          gender: "",
+          phone: "",
+          address: "",
         });
       } else {
         const alert = (
